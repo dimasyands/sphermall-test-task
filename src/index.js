171e6ac39bbb439ff2cf99eda7fc4189a4fc9491
dimasyands <dimasyan@gmail.com>
2019-02-12 10:43:46 +0200

    popup bug fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const popup = document.querySelector("#popup");
 const closePopup = popup.querySelector(".popup-close");
 const openPopup = popup.querySelector(".what-can-do-popup-button");
+const page = document.querySelector(".page");
 
 
 closePopup.addEventListener("click", function () {
   popup.classList.remove("show-modal");
+  page.classList.remove("scroll-off");
 });
 
 function onPopupOpenClick() {
   popup.classList.add("show-modal");
+  page.classList.add("scroll-off");
 }
 
 
